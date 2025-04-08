fc74326aedadd8cfa7c4d8fa15725a699b51ff6a
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-04-08 13:53:08 +0000

    fix UT

--- test_gemm_fp8.py
+++ test_gemm_fp8.py
@@ -36,8 +36,9 @@ block_size = 2
 
 scales_block_size = [block_size, block_size]
 
-has_bias = True
-M, K, N = 2, 16, 32
+# has_bias = True
+has_bias = False
+M, K, N = 32, 32, 16
 fp8_max = 448.0
 
 model = Mod(K, N, has_bias).eval()
@@ -58,6 +59,8 @@ scales = torch.where(scales == 0, torch.ones_like(scales), scales)  # avoid divi
 
 # Step 3: quantize → FP8
 q_blocks = (weight_blocks / scales).to(torch.float8_e4m3fn)
+q_blocks_reshape = q_blocks.permute(0, 2, 1, 3).contiguous()
+q_blocks_reshape = q_blocks_reshape.view(N, K)
 
 # Step 4: dequantize
 dq_blocks = q_blocks.float() * scales  # back to float32
@@ -68,7 +71,7 @@ w_dq = dq_blocks.view(N, K).to(compute_dtype)
 
 # Step 6: forward pass
 if has_bias:
-    bias = model.linear.bias.to(compute_dtype)
+    bias = model.linear.bias
     output1 = torch.matmul(data.to(compute_dtype), w_dq.T) + bias
 else:
     output1 = torch.matmul(data.to(compute_dtype), w_dq.T)
@@ -77,7 +80,7 @@ else:
 # TODO: test case with tail
 scales_squeeze = scales.view(N // block_size, K // block_size)
 output2 = sgl_kernel.cpu.fp8_scaled_mm(
-    data, q_blocks, scales_squeeze, scales_block_size, bias if has_bias else None, data.dtype, is_vnni=False
+    data, q_blocks_reshape, scales_squeeze, scales_block_size, bias if has_bias else None, data.dtype, is_vnni=False
 )
 print(output1)
 print(output2)
