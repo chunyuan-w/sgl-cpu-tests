d34852dfc429a755ec379b8e3c345e62bfeaa1fd
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-04-08 21:08:26 +0000

    add compare func

--- test_gemm_fp8.py
+++ test_gemm_fp8.py
@@ -4,6 +4,31 @@ import torch.nn as nn
 import sgl_kernel
 import sgl_kernel.cpu
 
+pres = {
+    torch.bfloat16 : 1e-2,
+    torch.float16: 1e-3,
+    torch.float32: 1e-5,
+}
+
+def compare(a: torch.Tensor, b: torch.Tensor, debug=False):
+
+    atol = rtol = pres[a.dtype]
+
+    res = torch.allclose(a, b, rtol=rtol, atol=atol)
+
+    max_diff = (a - b).abs().max().item()
+    max_index = torch.argmax((a-b).abs()).item()
+    a_sum = a.sum().item()
+    b_sum = b.sum().item()
+
+    if debug:
+        print(a)
+        print(b)
+        print(max_index, a.flatten()[max_index], b.flatten()[max_index])
+
+    print("Comparing: ", res, " max_diff = {:.5f}, asum = {:.3f}, bsum = {:.3f}".format(max_diff, a_sum, b_sum))
+
+
 torch.manual_seed(0)
 
 #   "quantization_config": {
@@ -132,5 +157,7 @@ print(output2[0])
 data_for_bmm = data.unsqueeze(0)
 weight_for_bmm = w_dq.T.contiguous().unsqueeze(0)
 
-import flashinfer
+compare(output1, output2)
+
+# import flashinfer
 # assert output1 == output2
