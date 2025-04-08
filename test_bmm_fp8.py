0b25d39ed0bd41dfc364bd5e80a6ca834d4593ae
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-04-08 21:04:21 +0000

    fix orginal fp8 bmm UT

--- test_bmm_fp8.py
+++ test_bmm_fp8.py
@@ -30,7 +30,7 @@ def input_to_float8(x, dtype = torch.float8_e4m3fn):
 
 def native_bmm_fp8(matA, matB, s, matC):
     bmm_out = torch.bmm(matA, matB.to(torch.bfloat16)) * s
-    matC = bmm_out
+    matC.copy_(bmm_out)
 
     return matC
 
@@ -45,6 +45,7 @@ def run_single_test(B, M, N, K, chunk=False, dtype=torch.bfloat16):
         matA = torch.randn(M, B, K + 64, dtype=dtype).narrow(2, 0, K).transpose_(0, 1)
         matB = torch.randn(B, N, K, dtype=dtype).transpose_(1, 2)
         matC = torch.randn(M, B, N + 64, dtype=dtype).narrow(2, 0, N).transpose_(0, 1)
+        ref_q = torch.randn(M, B, N + 64, dtype=dtype).narrow(2, 0, N).transpose_(0, 1)
     else:
         matA = torch.randn(M, B, K, dtype=dtype).transpose_(0, 1)
         matB = torch.randn(B, N, K, dtype=dtype).transpose_(1, 2)
@@ -71,6 +72,38 @@ def run_single_test(B, M, N, K, chunk=False, dtype=torch.bfloat16):
     assert matC.sum().item() == 0
     bmm_opt(matC, matA, packed_B, True)
     compare(ref, matC)
+    
+    ref_q.zero_()
+    assert ref_q.sum().item() == 0
+    native_bmm_fp8(matA, matB_q, matB_s, ref_q)
+    print(ref_q[0][0][0])
+    
+    matC.zero_()
+    assert matC.sum().item() == 0    
+    from flashinfer import _kernels
+    matB_q = matB_q.transpose(1, 2).contiguous()
+    _kernels.bmm(matC, matA, matB_q, False, matB_s)
+    print("my fp8 flashinfer compare")
+    compare(ref_q, matC)
+    print(ref[0][0][0])
+    
+    
+    matC.zero_()
+    assert matC.sum().item() == 0  
+    import sgl_kernel
+    import sgl_kernel.cpu
+    sgl_kernel.cpu.fp8_scaled_mm(
+        matC, matA, matB_q, is_vnni=False, scales2=matB_s
+    )
+    print("my fp8 sgl-kernel compare")
+    compare(ref_q, matC)
+    print(ref[0][0][0])
+    
+    
+    
+    
+    
+    
 
 
 def run_single_bench(B, M, N, K, chunk=False, dtype=torch.bfloat16):
@@ -118,14 +151,14 @@ def run_single_bench(B, M, N, K, chunk=False, dtype=torch.bfloat16):
     print("### bmm benchmark: B = {}, M = {}, N = {}, K = {}; bmm(bf16): {:.3f} us, bmm(native fp8): {:.3f} us, bmm (opt bf16): {:.3f} us".format(B, M, N, K, tt1, tt2, tt3))
 
 
-for m in [1, 2, 11, 111]:
-    run_single_test(17, m, 128 + 32, 512 + 32, chunk=True)
-    run_single_test(16, m, 512, 128 + 32, chunk=True)
+# for m in [1, 2, 11, 111]:
+#     run_single_test(17, m, 128 + 32, 512 + 32, chunk=True)
+#     run_single_test(16, m, 512, 128 + 32, chunk=True)
 
 run_single_test(1, 5, 64 + 32, 128 + 32, chunk=True)
 
-run_single_bench(16, 1, 512, 128, chunk=True)
-run_single_bench(16, 1, 128, 512, chunk=True)
+# run_single_bench(16, 1, 512, 128, chunk=True)
+# run_single_bench(16, 1, 128, 512, chunk=True)
 #run_single_bench(16, 4, 512, 128, chunk=True)
 #run_single_bench(16, 4, 128, 512, chunk=True)
 #run_single_bench(16, 8, 512, 128, chunk=True)
